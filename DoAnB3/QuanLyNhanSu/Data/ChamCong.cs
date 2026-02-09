adfb6993649b472aedd4328f96a7b49fffb693f7
HuynhNhu0104 <nhu_dth235723@student.agu.edu.vn>
2026-02-09 14:05:17 +0700

    Da em nop bai a

---
+++ DoAnB3/QuanLyNhanSu/Data/ChamCong.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSu.Data
+{
+    public class ChamCong
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public int Thang { get; set; }
+        public int Nam { get; set; }
+        public int SoNgayCong { get; set; }
+
+        public int NhanVienId { get; set; }
+        public virtual NhanVien NhanVien { get; set; }
+
+    }
+}
